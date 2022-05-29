e77cddbec468b2fe9782c3e662a62bc642c99575
blocallee <blocallee@gmail.com>
2022-05-29 21:37:44 +0900

    add 작업중

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react';
+import Home from './pages/Home';
 
 const App: FC = () => {
-  return <div className="App">ToDoList</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 };
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,42 @@
-export {};
+import React, { FC, useState } from 'react';
+
+const Home: FC = () => {
+  const [value, setValue] = useState('');
+  const [text, setText] = useState('');
+
+  const handleTextArea = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault;
+    setValue(e.target.value);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      if (value === '') {
+        return alert('내용 입력하라고');
+      } else {
+        setText(value);
+      }
+    }
+  };
+  const ListItem: FC = () => {
+    return <li>{text}</li>;
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        id="inputText"
+        placeholder="할 일 입력해"
+        arai-lable="할 일 입력하세요"
+        value={value}
+        onChange={handleTextArea}
+        onKeyPress={handleKeyPress}
+      />
+      <ul>
+        <ListItem />
+      </ul>
+    </form>
+  );
+};
+export default Home;
